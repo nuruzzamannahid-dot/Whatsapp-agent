c869737e086d4b038282001d36c67b5d8bcced19
nuruzzamannahid-dot <nuruzzaman.nahid@carrybee.com>
2026-08-24 18:01:06 +0600

    Update server.js

--- server.js
+++ server.js
@@ -45,7 +45,17 @@ const client = new Client({
   }),
   puppeteer: {
     headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-gpu',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process',
+      '--disable-extensions'
+    ]
   }
 });
 
